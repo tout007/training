b48a8d5a25582d1ffb4a4ba05f4a55994a4ee4cc
banti <banti.tout@gmail.com>
2020-10-20 15:24:54 +0530

    keep trying

---
+++ operator.rb
@@ -0,0 +1,12 @@
+  
+ puts(10 + 20)      
+ puts("subtract operator")   
+ puts(35 - 15)    
+ puts("multiply operator")   
+ puts(4 * 8)   
+ puts("division operator")   
+ puts(25 / 5)   
+ puts("exponential operator")   
+ puts(5 ** 2)   
+ puts("modulo operator")   
+ puts(25 % 4)   
