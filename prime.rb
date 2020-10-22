ca52d0a86db37b7c7b83ee93dff9c777e6e75b34
tout007 <60217143+tout007@users.noreply.github.com>
2020-10-22 19:55:41 +0530

    Shubham: indentation issue fixed

--- prime.rb
+++ prime.rb
@@ -1,20 +1,20 @@
 class Prime
-	def checkprime
-		    flag = 0
-		    puts "enter any number"
-		    number = gets.chomp.to_i;
-		    i = 2
-		  for i in i..number-1
-		  	 if number % i == 0
-		 	    	flag = 1
-		  	 end
-		  end   
-	   	   if flag == 1
-		 	     puts " #{number} is not prime"
-		     else
-			     puts " #{number} is prime"  
-	    	 end 
+  def check_prime
+    flag = 0
+    puts 'enter any number'
+    number = gets.chomp.to_i
+    i = 2
+    for i in i..(number - 1) do
+      if number % i == 0
+        flag = 1
+      end
     end
+    if flag == 1
+      puts "#{number} is not prime"
+    else
+      puts "#{number} is prime"
+    end
+  end
 end
 obj = Prime.new()
-obj.checkprime()
+obj.check_prime()
