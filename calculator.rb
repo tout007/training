a6f599bc0194e6c6d279ff8893198744d63af902
banti <banti.tout@gmail.com>
2020-10-26 10:33:39 +0530

    modified calculator

--- calculator.rb
+++ calculator.rb
@@ -3,27 +3,40 @@ class Percentage
       @numbers,@subjects = numbers, subjects
       num = @numbers.values
       sum = 0
+      grand_total = @subjects*100
       num.each do |a| 
         sum+=a 
       end
       
-      return sum/@subjects
+      return (sum/grand_total)*100
     end
-end
+    
+    def get_details()
+        puts "enter number of student to show result"
+        student = gets.chomp().to_i
+        students = {}
 
-puts "enter total number of subject"
-subjects =  gets.chomp().to_i
-numbers = {}
+        for j in (0..student-1) do
+            puts "enter your name"
+            name = gets.chomp().to_s
+            puts "enter total number of subject"
+            subjects =  gets.chomp().to_i
+            numbers = {}
+            
+            for i in (0..subjects-1) do 
+                puts "enter name of subject"
+                a = gets.chomp().to_s
+                puts "enter total marks obtain in #{a}"
+                number = gets.chomp().to_f
+                numbers[a] = number
+            end
+            students [name] = numbers
+            obj = Percentage.new()
+            puts "#{name},your percentage is #{obj.calculate(numbers,subjects)}"
+            puts " "
+        end  
+    end 
+end 
+obj1 = Percentage.new()
+obj1.get_details
 
-for i in (0..subjects-1) do 
-  puts "enter name of subject"
-  a = gets.chomp().to_s
-  puts "enter total number in #{a}"
-  number = gets.chomp().to_f
-
-  numbers[a] = number
-
-end
-obj = Percentage.new()
-puts "your percentage is #{obj.calculate(numbers,subjects)}" 
-  
