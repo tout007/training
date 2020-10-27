98fc8a91929040d6f97ba429f422a605fa0df1ed
banti <banti.tout@gmail.com>
2020-10-27 16:25:59 +0530

    dynamic calculator for calculation

---
+++ dynamic_calculator.rb
@@ -0,0 +1,46 @@
+class Calculator
+    def calculator()
+        obj3 = Calculator.new()
+        obj3.info()   
+    end
+
+    def info
+        puts "enter two number for calculation"
+        puts "enter first number"
+        a = gets.chomp().to_f
+        puts "enter second number"
+        b = gets.chomp().to_f
+        puts "enter operator for perform operation \n like +, -, *, /, % "
+        input = gets.chomp()
+        case input
+        when "+"
+            puts "#{a+b}"
+        when "-"
+            puts "#{a-b}"
+        
+        when "*"
+            puts "#{a*b}"
+            
+        when "/"
+            puts "#{a/b}"
+        
+        when "%"
+            puts "#{a%b}"    
+            
+        else
+            puts "you enter invalid key "
+            
+        end
+
+        puts "for more calculation enter 1 and for exit enter any key"
+        one = gets.chomp().to_i
+        if one ==1
+            obj2 = Calculator.new()
+            obj2.calculator()
+        end
+
+    end
+end
+
+obj = Calculator.new()
+puts obj.info
