a0e0df72f5a4b5c3322877b9f3a8c8bb3c409d60
banti <banti.tout@gmail.com>
2020-10-29 16:59:20 +0530

    modified

--- floyed_triangle.rb
+++ floyed_triangle.rb
@@ -1,21 +1,19 @@
 
-    class Floyed
-        def initialize()
-            puts "enter number of row"
-            row = gets.to_i
-            a = number = 1
-            while a <= row
-                b = 1
-                while b <= a
-                    print "#{number}  "
-                    number += 1
-                    b += 1
-                end
-                puts
-                a += 1
+class Floyed
+    def initialize()
+        puts "enter number of row"
+        row = gets.to_i
+        a = number = 1
+        while a <= row
+            b = 1
+            while b <= a
+                print "#{number} "
+                number += 1
+                b += 1
             end
+            puts
+            a += 1
         end
-
     end
-    obj = Floyed.new
-    
+end
+obj = Floyed.new
