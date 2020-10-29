a552bb8bd4b65fbb19e8b8781d10353976b32b98
sanjay <sanjay.tout@gmail.com>
2020-10-29 18:58:37 +0530

    a program of calculate Simple interest and compound interest

--- interest.rb
+++ interest.rb
@@ -1,27 +1,27 @@
 class Intrest
-     def get_inputs
-          puts "Enter principal balance,rate and time"
+	 def get_inputs
+	 	      puts "Enter principal balance,rate and time"
           puts "Enter value of principal balance"
-          @P = gets.chomp.to_f
+          @p = gets.chomp.to_f
           puts " Enter value of rate "
-          @R = gets.chomp.to_f
+          @r = gets.chomp.to_f
           puts "Enter value of time-duration"
-          @T = gets.chomp.to_f  
-     end
+          @t = gets.chomp.to_f  
+      end
      def simple_interest
           puts "Simple_interest is given below"
-          simple_interest = (@P * @R * @T) / 100
+          simple_interest = (@p * @t * @t)/100
           puts "Simple_interest is #{simple_interest}"    
      end
      def compund_interest
           puts "Compund_interest is given below"
-          c = 1 + @R / 100
-          amount = @P * c ** @T
+          c = 1 + @r / 100
+          amount = @p * c **@t
           puts "amount is #{amount}"
-          compund_interest = amount - @P  
+          compund_interest = amount - @p	
           puts "compund_interest is #{compund_interest}"
-     end  
-end  
+     end	
+end	 
 obj_interest = Intrest.new()
 obj_interest.get_inputs()
 obj_interest.simple_interest()
