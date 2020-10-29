4fbb6f1e12ea9d5e2a84d8727cc6b226f335b53d
banti <banti.tout@gmail.com>
2020-10-29 13:08:00 +0530

    modified

--- array_rearrangement.rb
+++ array_rearrangement.rb
@@ -1,17 +1,27 @@
-class Array
+class Mrray
     def arrangment(array) 
       new_array = []
       array1 = array.sort
-      puts "given array #{array1} "
-      n = array1.size
-      for i in 1..n/2
-        a ,b = array1.pop , array1.shift         
+      puts "given array after sorting #{array1} "
+      n = array1.length
+      loop do
+        a = array1.pop 
+        break if array1.empty? 
+        b = array1.shift
+        break if array1.empty?         
         new_array.push(a)                           
         new_array.push(b)
       end  
       puts "after rearrange #{new_array} " 
     end 
-   end 
-   array = gets.chomp().split(" ")  
-   obj = Array.new()
+end 
+   array = Array.new()
+   puts "enter array range"
+   n = gets.chomp().to_i
+   for i in (0..n-1)
+    array << gets.chomp().to_i
+   end  
+   obj = Mrray.new()
    obj.arrangment(array)
+
+   
