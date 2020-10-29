365c6428e1bf18ccdc7dc6768d421fb870000ccb
banti <banti.tout@gmail.com>
2020-10-29 16:55:03 +0530

    modified code

--- floyed_triangle.rb
+++ floyed_triangle.rb
@@ -1,18 +1,4 @@
 
-	# int rows, a,  b, number = 1;
-	# 
-	# printf("Number of rows of Floyd's triangle to print:");
-	# scanf("%d",&rows);
-	# 
-	# for ( a = 1 ; a <= rows ; a++ ) {
-		# for ( b = 1 ; b <= a ; b++ ) {
-			# printf("%d ", number);
-			# number++;
-		# }
-		# printf("\n");
-    # }
-    
-
     class Floyed
         def initialize()
             puts "enter number of row"
