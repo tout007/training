5e69f47aed2993a550cd54e6d72e5809c69e81a0
sanjay <sanjay.tout@gmail.com>
2020-10-30 17:54:00 +0530

    a program to check greater number among two number without useing < lessthan > greater than sign after modification

--- Greater.rb
+++ Greater.rb
@@ -1,20 +1,24 @@
 class CheckGreater
-     def get_inputs
+    def get_inputs
         puts "Please Enter any two number"
         puts "Enter First Number"
         @a = gets.chomp.to_i
         puts "Enter Second Number"
         @b = gets.chomp.to_i
-     end
-     def check_number
-        if @a/@b == 0
-           puts "#{@b} is Greater"
-        elsif @b/@a == 1
-          puts "Both Number are equal" 
-        else
-           puts "#{@a} is Greater"     
-        end
-     end 
+    end
+    def check_number
+       begin
+         if @a/@b == 0
+            puts "#{@b} is Greater"
+         elsif @b/@a == 1
+           puts "Both Number are equal" 
+         else
+            puts "#{@a} is Greater"     
+         end
+       rescue Exception => e
+          puts " #{@a} is Greater "
+       end
+    end 
 end 
 obj_check = CheckGreater.new()
 obj_check.get_inputs()
