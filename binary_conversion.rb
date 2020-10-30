6075e434240360c8040efb70342b9276fab35d11
sanjay <sanjay.tout@gmail.com>
2020-10-30 18:43:01 +0530

    a program of decimal to binary conversion after code optimise

--- binary_conversion.rb
+++ binary_conversion.rb
@@ -3,20 +3,17 @@ class BinaryConversion
     puts "please enter any number which you convert decimal to binary"
     @number = gets.chomp.to_i
     @array = Array.new(@number) #for store binary number
+    decimal_to_binary()
   end
   def decimal_to_binary
-    for i in 0..(@array.size-1) 
-      @array[i]  = @number % 2
-      @number /= 2
-    end 
-    puts "display the binary number"
-    j = @array.size
+     (0..@array.size-1).each{|i|@array[i] = @number % 2
+      @number /= 2}
+      puts "display the binary number"
+      j = @array.size
     while j >= 0
       print @array[j] 
       j -= 1
     end   
   end
 end
-conversion_obj = BinaryConversion.new() 
-conversion_obj.get_number()
-conversion_obj.decimal_to_binary()
+BinaryConversion.new().get_number()
