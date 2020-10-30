139243a7218f0558df9fe57b8d8c8aa533e9bfe4
sanjay <sanjay.tout@gmail.com>
2020-10-30 15:35:32 +0530

    a program of decimal to binary conversion after modification

--- binary_conversion.rb
+++ binary_conversion.rb
@@ -1,22 +1,22 @@
 class BinaryConversion
-    def get_input
-        puts "please enter number"
-        @number = gets.chomp.to_i
-        @array = Array.new(@number)
-    end
-    def Decimal_to_binary
-        for i in 0..(@array.size-1) 
-            @array[i]  = @number % 2
-                 @number /= 2
-        end 
-        puts "binary number is given below"
-        j = @array.size
-        while j >= 0
-            print @array[j] 
-          j -= 1
-        end   
-    end
+  def get_number
+    puts "please enter any number which you convert decimal to binary"
+    @number = gets.chomp.to_i
+    @array = Array.new(@number) #for store binary number
+  end
+  def decimal_to_binary
+    for i in 0..(@array.size-1) 
+      @array[i]  = @number % 2
+      @number /= 2
+    end 
+    puts "display the binary number"
+    j = @array.size
+    while j >= 0
+      print @array[j] 
+      j -= 1
+    end   
+  end
 end
 conversion_obj = BinaryConversion.new() 
-conversion_obj.get_input()
+conversion_obj.get_number()
 conversion_obj.Decimal_to_binary()
