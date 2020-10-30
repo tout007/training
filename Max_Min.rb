c2b304c6b213609092150f9547668286d2b1e6e4
sanjay <sanjay.tout@gmail.com>
2020-10-30 18:34:49 +0530

    a program of find maximum and minimum number from unsorted array, after optimise code

--- Max_Min.rb
+++ Max_Min.rb
@@ -1,26 +1,21 @@
-class Check_Max_Min
-	def get_array
-      puts "enter size of array"
-      size = gets.chomp.to_i
-  	  @array = Array.new(size)
-      for i in 0..(@array.size-1)
-   	     @array[i] = gets.chomp.to_i
-      end
-	end	
-	def maximum
-  	   maximum_number = @array[0]
-       minimum_number = @array[0]
-      for i in 0..(@array.size-1)
-         if @array[i] > maximum_number
-         	   maximum_number = @array[i]
-         elsif @array[i] < minimum_number
-         	   minimum_number = @array[i]
-         end 
-      end
-      puts "Maximum number is  #{maximum_number}"
-      puts "Minimum number is  #{minimum_number}"
-  end
+class CheckMaxMin
+  def get_array
+    puts "enter size of array"
+    @array = Array.new(gets.chomp.to_i)
+     (0..@array.size-1).each {|i| @array[i] = gets.chomp.to_i} 
+    maximum
+  end 
+  def maximum
+    maximum_number = @array[0]
+    minimum_number = @array[0]
+    (0..@array.size-1).each do |j|
+      if @array[j] > maximum_number
+        maximum_number = @array[j]
+      elsif @array[j] < minimum_number
+        minimum_number = @array[j]
+      end 
+    end
+    puts "Maximum number is  #{maximum_number} and minimum_number is #{minimum_number}"
+  end 
 end
-max_min_obj = Check_Max_Min.new()
-max_min_obj.get_array()
-max_min_obj.maximum()
+CheckMaxMin.new().get_array()
