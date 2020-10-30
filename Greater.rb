a8a132240a46bc8d096bd4a68935afc1b827f8fa
sanjay <sanjay.tout@gmail.com>
2020-10-30 19:06:08 +0530

    a program of decimal to binary conversion after code more optimise

--- Greater.rb
+++ Greater.rb
@@ -1,22 +1,20 @@
 class CheckGreater
      def get_inputs
-        puts "Enter First Number"
+        puts "Enter First Number and second number for checking"
         @num1 = gets.chomp.to_i
-        puts "Enter Second Number"
         @num2 = gets.chomp.to_i
         check_number
      end
      def check_number
-       begin
-         if @num1/@num2 == 0
-            puts "#{@num2} is Greater"
-         elsif @num2/@num1 == 1
-           puts "Both Number are equal" 
-         else
-            puts "#{@num1} is Greater"     
-         end
-       rescue Exception => e
-          puts " #{@num1} is Greater "
+        begin if @num1/@num2 == 0 
+                puts "#{@num2} is Greater"
+              elsif @num2/@num1 == 1
+                puts "Both Number are equal" 
+              else
+                puts "#{@num1} is Greater"     
+              end
+        rescue Exception => e
+          puts "#{@num1} is Greater "
        end
      end 
 end 
