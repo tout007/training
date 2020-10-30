b8d811692fb8115cc1f909510ce751057f21d0c3
sanjay <sanjay.tout@gmail.com>
2020-10-30 20:37:38 +0530

    a program of find  greater number among two number optimiz again

--- Greater.rb
+++ Greater.rb
@@ -1,22 +1,20 @@
 class CheckGreater
-     def get_inputs
-        puts "Enter First Number and second number for checking"
-        @num1 = gets.chomp.to_i
-        @num2 = gets.chomp.to_i
-        check_number
-     end
-     def check_number
-        begin if @num1/@num2 == 0 
-                puts "#{@num2} is Greater"
-              elsif @num2/@num1 == 1
-                puts "Both Number are equal" 
-              else
-                puts "#{@num1} is Greater"     
-              end
-        rescue Exception => e
-          puts "#{@num1} is Greater "
-       end
-     end 
+	def get_inputs
+		puts "Enter First Number and second number for checking"
+		@num1 = gets.chomp.to_i
+    @num2 = gets.chomp.to_i
+    check_number
+	end
+	def check_number
+	 begin 
+       @num1/@num2 == 0 ? (puts "#{@num2} is Greater") : [@num2/@num1 == 1 ? (puts "Both Number are equal") : (puts"#{@num1} is Greater")]
+   rescue Exception => e
+      puts  "#{e} and #{@num1} is Greater" 
+   end
+	end 
 end 
 CheckGreater.new().get_inputs()
 
+
+
+           
