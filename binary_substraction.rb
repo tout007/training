4e5ea120487a93819cb71dc2fa6c137f03e24014
banti <banti.tout@gmail.com>
2020-10-31 12:27:53 +0530

    program of binary addtion and substraction

---
+++ binary_substraction.rb
@@ -0,0 +1,28 @@
+class Binaryconverte
+
+    def integeraddition(a,b)
+        a,b = a,b
+        c = a.to_i(2)
+        d = b.to_i(2)
+    return c-d
+    end
+    def converttobinary(integer)
+        binary = []
+        while integer > 0
+          binary << integer % 2
+          integer /= 2
+        end
+        binary = binary.reverse.join
+    return binary
+    end
+end
+# puts "enter a decimal number to convert in binary"
+# integer = gets.to_i
+puts "enter first binary number"
+a = gets.to_s
+puts "enter second binary number"
+b = gets.to_s
+obj = Binaryconverte.new()
+e = obj.integeraddition(a,b)
+puts "binary substraction is #{obj.converttobinary(e)}"
+# "1001".to_i(2) #=> 9
