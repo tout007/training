60383c752cf3672477b43fe1044934ea2b1128b3
banti <banti.tout@gmail.com>
2020-10-31 11:50:30 +0530

    optimize

--- max_min_number.rb
+++ max_min_number.rb
@@ -1,10 +1,6 @@
-puts "enter a number"
-a = gets.to_i
-puts "enter a another number"
-b = gets.to_i
-array = Array.new()
-array.unshift(a,b)
-array = array.sort
-puts "minimum number is #{array[0]}"
-puts "maximam number is #{array[-1]}"
+puts "enter two number"
+a ,b = gets.to_i , gets.to_i 
+array = Array.new().unshift(a,b).sort
+puts "#{array[0]} is small and #{array[-1]} is Greater"
+
    
